7c6ca7f7e0c3a917b446b3c09db33db70c0d11ee
rthorst <thorstadrs@gmail.com>
2016-09-23 08:40:18 -0400

    updated some columns

--- create_excel.py
+++ create_excel.py
@@ -1,5 +1,5 @@
 import csv, bridgetools
-
+import numpy as np
 
 # INPUT: hand   hand object
 #        idx    index of a player in list of players
@@ -37,34 +37,72 @@ def get_opening_bid(hand):
         else:
                 return bidding_no_passes[0]
 
+# given 2 hand objects, return IMP swing: positive value means NS gain IMPs.
+def calc_imp_swing(hand1, hand2):
+
+        # calculate point_swing. If negative reverse it and create boolean reverse_imp_swing = True
+        point_swing = (hand1.score - hand2.score)
+        reverse_imp_swing = False # if true flip the sign of point swing but return negative.
+        if point_swing < 0 : # negative imp swing? reverse sign but later change.
+                point_swing = -point_swing
+                reverse_imp_swing = True
+        # setup IMPS table.
+        min_scores = [0,20,50,90,130,170,220,270,320,370,430,500,600,750,900,
+                      1100,1300,1500,1750,2000,2250,2500,3000,3500,4000,99999999999]
+        imps = range(0,26)
+
+        # return IMP swing
+        for (min_score, max_score, imps) in zip(min_scores[:-1], min_scores[1:], imps[:-1]):
+                if ((point_swing >= min_score) & (point_swing < max_score)):
+                        imp_swing = imps
+                        if reverse_imp_swing:
+                                imp_swing = - imps
+                        return imp_swing
+
+
+
 ## VARS ##
 infile = "sample.lin"
 outfile = "sample_out.csv"
+head_to_head_match = True # if true, expects lin of 2 tables i.e. Hand#16 table 1 -- Hand#16 table 2 --
+                          #  Hand#16 table 1 etc
 
-# Read infile and parse
+# Read infile and parse, reshaping parse to pairs of same hand played at both tables.
 lin = open(infile, 'r').read()
 parse = bridgetools.parse_linfile(lin)
+parse = np.reshape(parse, (len(parse)/2, 2))
 
 # Setup outfilie and write header.
 of = open(outfile, 'wb')
 w = csv.writer(of)
-header = ['OPENER_SEAT','OPENERS_SIDE_WINS_CONTRACT','OPENING_IS_AT_1_LEVEL','A_PLAYER_FROM_TABLE']
+header = ['HAND_NUMBER','ROOM_NUMBER','OPENER_SEAT','OPENERS_SIDE_WINS_CONTRACT','OPENING_IS_AT_1_LEVEL',
+          'CONTRACT','IMP_SWING','A_PLAYER_FROM_TABLE']
 w.writerow(header)
 
-# Iterate over hands.
-for hand in parse:
+# Iterate over single hand played at both tables.
+hand_num = 1
+for (hand_table1, hand_table2) in parse:
 
-        # extract some info.
-        opener_seat = convert_idx_to_seat(hand, hand.opener) # range: {1,4}
-        opener_side_win_contract = return_whether_opener_side_win_contract(hand) # {0 or 1}
-        opened_at_1_level = int(get_opening_bid(hand)[0] == "1") # {0 or 1}
-        a_player_from_table = hand.players[0]
+        # extract info pertaining to both hands, i.e. IMP swing.
+        imp_swing = calc_imp_swing(hand_table1, hand_table2)
 
-        # write.
-        w.writerow([opener_seat,opener_side_win_contract, opened_at_1_level,
-                    a_player_from_table])
+        # extract info pertaining to each hand individually and write IMP swing.
+        room_num = 1
+        for hand in (hand_table1, hand_table2):
+                opener_seat = convert_idx_to_seat(hand, hand.opener) # range: {1,4}
+                opener_side_win_contract = return_whether_opener_side_win_contract(hand) # {0 or 1}
+                opened_at_1_level = int(get_opening_bid(hand)[0] == "1") # {0 or 1}
+                a_player_from_table = hand.players[0]
+                contract = hand.contract[0]
 
-        
-        
+                # write.
+                w.writerow([hand_num, room_num, opener_seat,opener_side_win_contract, opened_at_1_level, contract,
+                            imp_swing, a_player_from_table])
+
+                # increment room number
+                room_num += 1
+
+        # increment hand num
+        hand_num += 1
         
         
